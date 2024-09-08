2a7b7207692ce8510362f37b4d045e6e5975a491
fanghaizhou <fanghaizhou611@163.com>
2024-09-08 21:45:38 +0800

    fix print

--- txn_entry.cc
+++ txn_entry.cc
@@ -3,10 +3,6 @@ std::string TxnEntry::to_string() {
     return fmt::format("txnId: {}\n"
                        "dataCnt: {}\n"
                        "frstDataSqno: {}\n"
-                       "lstDataSqno: {}\n"
-                       "msgSqno: {}\n"
-                       "msgCnt: {}\n"
-                       "timestamp: {}\n",
-                       txnId, dataCnt, frstDataSqno, lstDataSqno, msgSqno,
-                       msgCnt, timestamp);
+                       "lstDataSqno: {}\n",
+                       txnId, dataCnt, frstDataSqno, lstDataSqno);
 }
